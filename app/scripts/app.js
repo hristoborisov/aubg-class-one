bed2f8762273b72ca56ca2d575dcb61413171ed2
hristoborisov <raxevsky@gmail.com>
2014-11-18 00:08:20 +0200

    Init

---
+++ app/scripts/app.js
@@ -0,0 +1,8 @@
+var app = angular.module("Photos", ["ionic"]);
+
+var MainController = function ($scope) {
+    $scope.cities = ["Amsterdam", "Berlin", "Paris", "London", "Sofia",
+                     "Belgrade", "Athens", "Bern", "Chisinau", "Budapest",
+                    "Bucharest", "Kiev", "Madrid", "Oslo", "Podgorica", "Prague"];
+
+}
