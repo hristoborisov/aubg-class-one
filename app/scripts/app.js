a0e64dfef6de1fbee134785f6f4e49c27579184e
hristoborisov <raxevsky@gmail.com>
2014-11-19 22:37:44 +0200

    Adding cities as json object without files

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,26 +6,22 @@ var MainController = function ($scope)
       {  
           "IsCapital":true,
           "Name":"Sofia",
-          "Population":2000000,
-          "Picture":"2e17b340-702a-11e4-8370-5f09c71bb06c",
+          "Population":2000000
       },
       {  
           "IsCapital":false,
           "Name":"Blagoevgrad",
-          "Population":80000,
-          "Picture":"3c847ad0-702a-11e4-b64a-9d49c8a901f0",
+          "Population":80000
       },
       {  
           "IsCapital":false,
           "Name":"Varna",
-          "Population":600000,
-          "Picture":"45f8fb40-702a-11e4-b1e7-3f849ce065bb",
+          "Population":600000
       },
       {  
           "IsCapital":false,
           "Name":"Plovdiv",
-          "Population":350000,
-          "Picture":"4f20bb40-702a-11e4-b1e7-3f849ce065bb",
+          "Population":350000
       }
     ];
 }
