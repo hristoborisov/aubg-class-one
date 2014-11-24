c4f0a2c1fc546af42b4bfc30fdb1bca4c551e0e5
hristoborisov <raxevsky@gmail.com>
2014-11-24 22:20:48 +0200

    Included the new city view and controller

--- app/scripts/NewCityController.js
+++ app/scripts/NewCityController.js
@@ -1,5 +1,5 @@
 var NewCityController = function ($scope, $stateParams) {
-    //TODO6: Create new record in the database based on your new view
+    //TODO6: Create new record in the database based on your new view using the city variable
 };
 
 app.controller("NewCityController", DetailsController);
